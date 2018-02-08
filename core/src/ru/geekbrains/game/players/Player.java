4dcf7f629219d4fb7ccff444c0aad6329ea913ac
sol <solo-yes@mail.ru>
2018-02-09 09:50:48 +1000

    Player + master branch

--- core/src/ru/geekbrains/game/players/Player.java
+++ core/src/ru/geekbrains/game/players/Player.java
@@ -25,6 +25,7 @@ public class Player extends Sprite {
         tmp.set(target);
         setAngle(tmp.sub(pos).angle() - 90);
         //stars.setV();
+        
     }
 
     public Player(TextureAtlas atlas, StarsHandler stars) {
