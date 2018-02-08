238d0c8b2bc7db5ccd7d2d4bdc6ea09cd1fa3fc6
sol <solo-yes@mail.ru>
2018-02-09 09:48:33 +1000

    Now player can move smoothly

--- core/src/ru/geekbrains/game/players/Player.java
+++ core/src/ru/geekbrains/game/players/Player.java
@@ -19,13 +19,12 @@ public class Player extends Sprite {
     private Vector2 tmp;
     private Vector2 tmp2;
     private Vector2 tmp3;
-    private StarsHandler stars;
 
     public void setTarget(Vector2 target) {
         this.target = target;
         tmp.set(target);
         setAngle(tmp.sub(pos).angle() - 90);
-        stars.setV();
+        //stars.setV();
     }
 
     public Player(TextureAtlas atlas, StarsHandler stars) {
@@ -39,24 +38,28 @@ public class Player extends Sprite {
         target = new Vector2();
         norDestanation = new Vector2();
         destanation = new Vector2();
-        this.stars = stars;
     }
 
     public void update(float delta) {
-        tmp2.set(target);
-        destanation = tmp2.sub(pos);
-        tmp3.set(destanation);
-        norDestanation = tmp3.nor();
-        tmp3.set(norDestanation);
-
-        if (destanation.len() > delta) {
-            pos.mulAdd(tmp3, velocity * delta);
-            frame += 1;
-            frame %= 6;
-
-        } else {
-            pos.set(target);
+        //For smooth player manipulation
+        if  (!this.isMe(target)) {
+            tmp2.set(target);
+            destanation = tmp2.sub(pos);
+            tmp3.set(destanation);
+            norDestanation = tmp3.nor();
+            tmp3.set(norDestanation);
+            //Parking
+            if (destanation.len() > delta) {
+                pos.mulAdd(tmp3, velocity * delta);
+                frame += 1;
+                frame %= 6;
+            }
+            else pos.set(target);
+            //
+        }
+        else {
             frame = 0;
         }
+        //
     }
 }
