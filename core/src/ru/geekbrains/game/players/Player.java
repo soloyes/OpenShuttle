a940da0d28e797f6dc7a51f4f009d1a6cdaadf22
sol <solo-yes@mail.ru>
2018-02-09 11:26:53 +1000

    Starts now flying in contr-direction

--- core/src/ru/geekbrains/game/players/Player.java
+++ core/src/ru/geekbrains/game/players/Player.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.game.star.StarsHandler;
 import ru.geekbrains.stargame.engine.Sprite;
+import ru.geekbrains.stargame.engine.math.Rect;
 
 /**
  * Created by sol on 2/6/18.
@@ -19,13 +20,20 @@ public class Player extends Sprite {
     private Vector2 tmp;
     private Vector2 tmp2;
     private Vector2 tmp3;
+    private Rect player2TimesMore;
+    StarsHandler stars;
 
     public void setTarget(Vector2 target) {
         this.target = target;
         tmp.set(target);
         setAngle(tmp.sub(pos).angle() - 90);
-        //stars.setV();
-        
+        //if (!player2TimesMore.isMe(target))
+        //stars.setV(90 - pos.angle());
+        tmp.set(target);
+
+        float angle = tmp.sub(pos).angle();
+        System.out.println(angle);
+        stars.setV(angle);
     }
 
     public Player(TextureAtlas atlas, StarsHandler stars) {
@@ -39,9 +47,13 @@ public class Player extends Sprite {
         target = new Vector2();
         norDestanation = new Vector2();
         destanation = new Vector2();
+        this.stars = stars;
+        player2TimesMore = new Rect(this.pos.x, this.pos.y, getHeight()*2, getWidth()*2);
     }
 
     public void update(float delta) {
+        player2TimesMore.pos.set(this.pos);
+
         //For smooth player manipulation
         if  (!this.isMe(target)) {
             tmp2.set(target);
